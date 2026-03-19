0eea7da1b7e70d1b019fad8deff34acb2be143ed
Andrea Escolar <andrea.escolar.artigas@ibadia.cat>
2026-03-19 11:41:37 +0100

    crea los datos iniciales

---
+++ src/data/tasks.ts
@@ -0,0 +1,10 @@
+import type { Task } from "../types/task";
+
+export const tasks: Task[] = [
+    { id: 1, title: "Estudiar React" },
+    { id: 2, title: "Hacer la compra" },
+    { id: 3, title: "Preparar ejercicio de TypeScript" },
+    { id: 4, title: "Leer documentación de Bootstrap" },
+    { id: 5, title: "Practicar componentes" },
+    { id: 6, title: "Repasar useState" },
+]
